5dab35abd29c78ab50351d369031dec73e57b7e8
gerhard.kalab <gerhard.kalab@gmail.com>
2012-07-06 16:33:38 +0000

    return correct move for en passant moves

--- src/chesspresso/position/AbstractMoveablePosition.java
+++ src/chesspresso/position/AbstractMoveablePosition.java
@@ -14,40 +14,41 @@
 
 package chesspresso.position;
 
-
-import chesspresso.*;
-import java.util.*;
-import chesspresso.move.Move;
+import chesspresso.Chess;
 import chesspresso.move.IllegalMoveException;
-
+import chesspresso.move.Move;
 
 /**
- *
- * @author  Bernhard Seybold
+ * 
+ * @author Bernhard Seybold
  * @version $Revision: 1.1 $
  */
 public abstract class AbstractMoveablePosition extends AbstractMutablePosition
-    implements MoveablePosition
-{
-    
-    public void doMove(Move move) throws IllegalMoveException
-    {
-        doMove(move.getShortMoveDesc());
-    }
+		implements MoveablePosition {
 
-    public short getMove(int from, int to, int promoPiece)
-    {
-        if (getColor(from) != getToPlay()) return Move.ILLEGAL_MOVE;  // =====>
-        int piece = getPiece(from);
-        if (piece == Chess.PAWN) {
-            return Move.getPawnMove(from, to, Chess.sqiToCol(from) != Chess.sqiToCol(to), promoPiece);
-        } else if (piece == Chess.KING && (to - from) ==  2) {
-            return Move.getShortCastle(getToPlay());
-        } else if (piece == Chess.KING && (to - from) == -2) {
-            return Move.getLongCastle(getToPlay());
-        } else {
-            return Move.getRegularMove(from, to, !isSquareEmpty(to));
-        }
-    }
+	public void doMove(Move move) throws IllegalMoveException {
+		doMove(move.getShortMoveDesc());
+	}
+
+	public short getMove(int from, int to, int promoPiece) {
+		if (getColor(from) != getToPlay())
+			return Move.ILLEGAL_MOVE; // =====>
+		int piece = getPiece(from);
+		if (piece == Chess.PAWN) {
+			if (Chess.sqiToCol(from) != Chess.sqiToCol(to)
+					&& getPiece(to) == Chess.NO_PIECE) {
+				return Move.getEPMove(from, to);
+			} else {
+				return Move.getPawnMove(from, to,
+						Chess.sqiToCol(from) != Chess.sqiToCol(to), promoPiece);
+			}
+		} else if (piece == Chess.KING && (to - from) == 2) {
+			return Move.getShortCastle(getToPlay());
+		} else if (piece == Chess.KING && (to - from) == -2) {
+			return Move.getLongCastle(getToPlay());
+		} else {
+			return Move.getRegularMove(from, to, !isSquareEmpty(to));
+		}
+	}
 
 }
